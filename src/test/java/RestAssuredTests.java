6f965d56dcf1cf4ad5e9056f82fef570a8dbef78
DubineanschiT <tatiana.financiar@gmail.com>
2020-07-19 20:11:33 +0300

    added list task

--- src/test/java/RestAssuredTests.java
+++ src/test/java/RestAssuredTests.java
@@ -1,27 +1,32 @@
 import io.restassured.http.ContentType;
+import org.junit.Assert;
 import org.junit.Test;
-import java.util.HashMap;
-import java.util.Map;
+
+import java.util.*;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class RestAssuredTests {
-    private static final String baseUrl="https://jsonplaceholder.typicode.com/posts/";
+    private static final String baseUrl = "https://jsonplaceholder.typicode.com/posts/";
+
     @Test
-    public void validateSuccessfulResponseCode(){
-       get(baseUrl+"1").
-               then().statusCode(200).
-               and().
-               body("body", containsString("reprehenderit molestiae ut ut quas totam")).
-               and().
-               body("title", is("sunt aut facere repellat provident occaecati excepturi optio reprehenderit"));
+    public void validateSuccessfulResponseCode() {
+        get(baseUrl + "1").
+                then().statusCode(200).
+                and().
+                body("body", containsString("reprehenderit molestiae ut ut quas totam")).
+                and().
+                body("title", is("sunt aut facere repellat provident occaecati excepturi optio reprehenderit"));
     }
+
     @Test
-    public void validateThatListContainsIds(){
-        get(baseUrl).then().body("id",hasItems(43));
+    public void validateThatListContainsIds() {
+        get(baseUrl).then().body("id", hasItems(43));
     }
+
     @Test
-    public void postBody(){
+    public void postBody() {
         Map<String, Object> jsonAsMap = new HashMap<>();
         jsonAsMap.put("userId", 1);
         jsonAsMap.put("title", "TaniushaMaladet");
@@ -35,8 +40,9 @@ public class RestAssuredTests {
                 then().
                 statusCode(201);
     }
+
     @Test
-    public void putBody(){
+    public void putBody() {
         Map<String, Object> jsonAsMap = new HashMap<>();
         jsonAsMap.put("userId", 2);
         jsonAsMap.put("title", "PutMethod");
@@ -46,12 +52,26 @@ public class RestAssuredTests {
                 contentType(ContentType.JSON).
                 body(jsonAsMap).
                 when().
-                put(baseUrl+"1").
+                put(baseUrl + "1").
                 then().
                 statusCode(200);
     }
+
     @Test
-    public void deleteTest(){
-        when().delete(baseUrl+"1").then().statusCode(200);
+    public void deleteTest() {
+        when().delete(baseUrl + "1").then().statusCode(200);
+    }
+
+    @Test
+    public void validateList() {
+        List<User> userList = given()
+                .when()
+                .get(baseUrl)
+                .then()
+                .extract()
+                .body()
+                .jsonPath().getList(".", User.class);
+        Assert.assertEquals(100, userList.size());
+        Assert.assertTrue(userList.stream().anyMatch(x -> x.getId() != null && x.getTitle() != null));
     }
 }
